87486af2f3c07373b4e9a000f1b2c102a8589f08
Vert <81417219+VertCodes@users.noreply.github.com>
2021-04-02 00:10:57 +0100

    Uploaded script

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # xonotic-server-status
-Gets the status of a Xonotic server
+Gets the status of a Xonotic server and returns a dictionary object.
+
+Nabbed the ping packet data from wireshark and figured out the response manually. :)
+
+Originally created to see if certain maps were in play for the defrag gamemode.
+
+## Usage:
+
+Basic usage from the command line (prints hostname, mapname and players)
+
+```bash
+./xsstat.py host [port]
+```
+
+Using in python
+
+Available keys are:
+'players_data_array', 'gamename', 'modname', 'gameversion', 'sv_maxclients', 'clients', 'bots', 'mapname', 'hostname', 'protocol', 'qcstatus', 'd0_blind_id', 'players'
+
+With the exception of the player object, values are stores as a bytes object.
+
+Printing a player object, will display ping, player name and score. 
+
+Useful properties and methods of a player object:
+- player.ping
+- player.name
+- player.score_or_spec()
+
+```python
+import xsstat
+
+info = xsstat.ping("127.0.0.1", 26000)
+
+print(info['hostname'].decode())
+
+print(info['mapname'].decode())
+
+if info['mapname'].decode() == "map_name":
+    print("\a")
+    print("Its your favourite map, get a better time!")
+
+for player in info['players']:
+    print(player)
+```
+
+It's nice to combine with watch to keep up to date
+```bash
+watch -d -n 60 "./xsstat.py host [port]"
+```
---
+++ xsstat.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python3
+
+import re, signal, socket, sys
+
+TIMEOUT = 6
+XONOPINGBIN = bytes.fromhex("ffffffff676574737461747573")
+
+
+class Player:
+    'A player object, easier to work with'
+
+    def __init__(self, score, ping, raw_name):
+        self.score = score
+        self.ping = ping
+        self.raw_name = raw_name
+        self.name = self.get_name()
+
+    def __str__(self):
+        return f"{self.ping:>3} {self.get_name()[:32]:32} {self.score_or_spec()}"
+
+    def is_spectating(self):
+        if self.score == -666:
+            return True
+        return False
+
+    def score_or_spec(self):
+        if self.is_spectating():
+            return "Spectator"
+        return self.score
+
+    def get_name(self):
+        unicode_name = self.raw_name.decode("utf-8", "ignore")
+
+        color_code = r"\^x[0-9a-fA-F]{3}"
+        cd_code = r"\^\d"
+
+        no_color_codes = re.sub(color_code, "", unicode_name)
+        no_codes = re.sub(cd_code, "", no_color_codes)
+
+        return no_codes
+
+
+def dp_protocol_parse(data):
+
+    players = []
+    data_parts = data.rsplit("\n".encode())
+
+    info = {
+        'status_response': data_parts[0],
+        'players_data_array': data_parts[2:-1]
+    }
+
+    server_info_data = data_parts[1]
+    key_value_data_pairs = server_info_data.rsplit("\\".encode())
+
+    for i in range(2, len(key_value_data_pairs), 2):
+        info[key_value_data_pairs[i-1].decode("utf-8", "ignore")] = \
+            key_value_data_pairs[i]
+
+    for player_data in info['players_data_array']:
+        player_data_array = player_data.rsplit(" ".encode())
+        raw_name = player_data.rsplit('"'.encode())[1]
+
+        player = Player(
+                int(player_data_array[0]), int(player_data_array[1]),
+                raw_name)
+
+        players.append(player)
+
+    info['players'] = players
+
+    return info
+
+
+def handler(signum, frame):
+    raise TimeoutError(
+        f"Timed out after waiting {TIMEOUT} seconds for a response"
+    )
+
+
+def ping(host="127.0.0.1", uport=26000):
+    try:
+        port = int(uport)
+
+        if port <= 0 or port > 65535:
+            raise ValueError
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.sendto(XONOPINGBIN, (host, port))
+
+        signal.signal(signal.SIGALRM, handler)
+        signal.alarm(TIMEOUT)
+
+        while True:
+            data, addr = s.recvfrom(1024)
+
+            if addr[0] == host:
+                return dp_protocol_parse(data)
+
+    except ValueError:
+        print("Port must be an integer value in range 0-65535")
+        sys.exit(1)
+    except Exception as e:
+        print(f"ERROR: {e}")
+        sys.exit(1)
+
+
+def display(info):
+    
+    print(info['hostname'].decode())
+    print(info['mapname'].decode())
+
+    for player in info['players']:
+        print(player)
+
+
+def __main__():
+    host = "127.0.0.1"
+    port = 26000
+
+    if len(sys.argv) > 1:
+        host = sys.argv[1]
+
+    if len(sys.argv) > 2:
+        port = sys.argv[2]
+            
+    display(ping(host, port))
+
+
+if __name__ == "__main__":
+            __main__()
